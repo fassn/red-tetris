9b658f35e4679a33f5858e40ea8b619dc4bff7d3
Christopher Fargere <christopher.fargere@gmail.com>
2026-04-11 07:28:31 +0200

    refactor: remove GitHub link from footer and adjust layout

--- components/footer.tsx
+++ components/footer.tsx
@@ -3,9 +3,7 @@ import ThemeToggle from './theme-toggle'
 const Footer = () => {
     return (
         <footer className='flex w-full h-10 shrink-0 bg-brand px-4 justify-between items-center z-10'>
-            <a href='https://github.com/fassn/red-tetris' target='_blank' rel='noopener noreferrer' className='text-sm hover:underline' aria-label='GitHub repository'>
-                GitHub
-            </a>
+            <div className='w-8' />
             <div className='text-center uppercase text-sm'>© 2026 fassn</div>
             <ThemeToggle />
         </footer>
